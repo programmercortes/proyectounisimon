b79bd35e657122765c5a140d56cdc5d25833fb68
Pablo Cortes <p_cortes@unisimon.edu.co>
2022-10-19 11:19:52 -0500

    Version 3.0
    Date: Wedneday 19th of october, 2022.
    Time: 11:20 am.
    Add player with is rigidbody and collider. Also its texture.

---
+++ UnisimonProjectPcortes/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float maxSpeed = 6.0f;
+    public float moveDirection;
+    public bool fascingRight = true;
+    private
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
